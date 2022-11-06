4361ca7627dce4b59de87a7eafd74fa5e79151bb
Basma Siam <basma.siam@gmail.com>
2022-11-06 08:59:59 -0800

    It will remove un-neccessary space

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,28 @@
-
 window.onload=function(){
   let form = document.querySelector("form");
   form.onsubmit=function(event){
   event.preventDefault();  
 
+  //getting the input values from the users 
   const name = document.getElementById("userName").value;
   const age = document.getElementById("userAge").value;
   const question1Answer = document.querySelector('input[name="question1"]:checked').value;
   const question2Answer = document.querySelector('input[name="question2"]:checked').value;
   const question3Answer = document.querySelector('input[name="question3"]:checked').value;
   const question4Answer = document.querySelector('input[name="question4"]:checked').value;
+
   document.querySelector("span#name").innerText = name;
+
   let recommendation = getRecommendation(question1Answer,question2Answer,question3Answer,question4Answer); 
   document.querySelector("h2#language").innerText = recommendation;
   document.getElementById("recommendation").removeAttribute("class");
   document.getElementById("form").setAttribute("class","hidden");
-  console.log("hi");
   };
-}  
-function getRecommendation(answer1,answer2,answer3,answer4){
-    
-    /*let python = document.getElementById("python");
-    let js = document.getElementById("javaScript");
-    let swift = document.getElementById("swift");
-    let cSharp = document.getElementById("cSharp");*/
-  
+}
+//This function take 4 ansewers as a parameter and return the recommended language as a string.
+function getRecommendation(answer1,answer2,answer3,answer4){ 
   if(answer2==="data" ){
       return "Python"
-
     }  
     else if(answer2==="mobile"){
       return "Swift";
@@ -40,6 +35,9 @@ function getRecommendation(answer1,answer2,answer3,answer4){
     }
     else if(answer4==="yes"){
       return "Python";
+    }
+    else{
+      return "Java Script";
     }
 }
 

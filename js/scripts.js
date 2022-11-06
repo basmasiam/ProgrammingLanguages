3b00dad4e83f3add12ce632fb52556a7184dfe7b
Basma Siam <basma.siam@gmail.com>
2022-11-06 01:55:35 -0700

    It will add logic to the function getRecommendation to return a result for user

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,45 @@
+
 window.onload=function(){
   let form = document.querySelector("form");
   form.onsubmit=function(event){
   event.preventDefault();  
 
+  const name = document.getElementById("userName").value;
+  const age = document.getElementById("userAge").value;
+  const question1Answer = document.querySelector('input[name="question1"]:checked').value;
+  const question2Answer = document.querySelector('input[name="question2"]:checked').value;
+  const question3Answer = document.querySelector('input[name="question3"]:checked').value;
+  const question4Answer = document.querySelector('input[name="question4"]:checked').value;
+  document.querySelector("span#name").innerText = name;
+  let recommendation = getRecommendation(question1Answer,question2Answer,question3Answer,question4Answer); 
+  document.querySelector("h2#language").innerText = recommendation;
+  document.getElementById("recommendation").removeAttribute("class");
+  document.getElementById("form").setAttribute("class","hidden");
+  console.log("hi");
   };
-
+}  
+function getRecommendation(answer1,answer2,answer3,answer4){
+    
+    /*let python = document.getElementById("python");
+    let js = document.getElementById("javaScript");
+    let swift = document.getElementById("swift");
+    let cSharp = document.getElementById("cSharp");*/
+  
+  if(answer2==="data" ){
+      return "Python"
+    }  
+    else if(answer2==="mobile"){
+      return "Swift";
+    } 
+    else if(answer3==="back"){
+      return "C#";
+    }
+    else if(answer1==="fun" && answer3==="front"){
+      return "Java Script";
+    }
+    else if(answer4==="yes"){
+      return "Python";
+    }
 }
+
+
